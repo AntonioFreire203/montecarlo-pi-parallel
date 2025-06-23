e35cf601f6bef446253d438b316c48e6a17f2c31
AntonioFreire203 <freireantonio203@gmail.com>
2025-06-23 09:51:34 -0300

    feat:salvar os dados em arquivo csv

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,36 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        int totalPoints = 10_000_000; // Número total de pontos a serem gerados
+        int totalPoints = 10_000_000;
         int[] threadCounts = {1, 2, 4, 8};
 
-        for (int threads : threadCounts) {
-            MonteCarloPiParallel calculator = new MonteCarloPiParallel(totalPoints, threads);
-            long startTime = System.nanoTime();
-            double piEstimate = calculator.calculatePi();
-            long endTime = System.nanoTime();
 
-            double durationInSeconds = (endTime - startTime) / 1_000_000_000.0;
-            System.out.printf("Threads: %d | Pi: %.6f | Time: %.3f s%n", threads, piEstimate, durationInSeconds);
+        File resultsDir = new File("C:\\Users\\anton\\IdeaProjects\\pi-montecarlo-parallel\\dados");
+        if (!resultsDir.exists()) {
+            resultsDir.mkdir();
+        }
+
+
+        try (PrintWriter writer = new PrintWriter(new FileWriter("C:\\Users\\anton\\IdeaProjects\\pi-montecarlo-parallel\\dados\\dados.csv"))) {
+            writer.println("threads,pi,time_seconds");
+
+            for (int threads : threadCounts) {
+                MonteCarloPiParallel calculator = new MonteCarloPiParallel(totalPoints, threads);
+                long startTime = System.nanoTime();
+                double piEstimate = calculator.calculatePi();
+                long endTime = System.nanoTime();
+
+                double durationInSeconds = (endTime - startTime) / 1_000_000_000.0;
+                writer.printf("%d,%.6f,%.3f%n", threads, piEstimate, durationInSeconds);
+                System.out.printf("Threads: %d | Pi: %.6f | Time: %.3f s%n", threads, piEstimate, durationInSeconds);
+            }
+            System.out.println("Resultados salvos em dados/dados.csv");
+        } catch (IOException e) {
+            System.out.println("Erro ao salvar os  dados: " + e.getMessage());
         }
     }
 }
